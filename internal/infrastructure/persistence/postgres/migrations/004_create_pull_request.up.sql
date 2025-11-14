8b6411033d5dc7f48e805acd7b4f20fde2c54ada
shirr9 <simkagaechka1@gmail.com>
2025-11-14 20:23:37 +0300

    update

--- internal/infrastructure/persistence/postgres/migrations/004_create_pull_request.up.sql
+++ internal/infrastructure/persistence/postgres/migrations/004_create_pull_request.up.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS pull_request (
     title VARCHAR(255) NOT NULL,
     author_id  VARCHAR(255),
     status pr_status DEFAULT 'OPEN',
-    need_more_reviewers BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     merged_at TIMESTAMP WITH TIME ZONE,
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
