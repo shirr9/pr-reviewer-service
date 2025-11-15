da25ea6cf0ac1fd58d2500904a171c2ce6c70d90
shirr9 <simkagaechka1@gmail.com>
2025-11-16 01:38:14 +0300

    feature: add server settings

--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -1,9 +1,20 @@
 package config
 
+import "time"
+
 // Config represents the application configuration.
 type Config struct {
-	Env        string `yaml:"env" env-default:"local"`
-	PostgresDb `yaml:"postgres"`
+	Env        string     `yaml:"env" env-default:"local"`
+	Server     Server     `yaml:"server"`
+	PostgresDb PostgresDb `yaml:"postgres"`
+}
+
+// Server contains HTTP server configuration.
+type Server struct {
+	Port         int           `yaml:"port" env-default:"8080"`
+	Env          string        `yaml:"env" env-default:"local"`
+	ReadTimeout  time.Duration `yaml:"read_timeout" env-default:"10s"`
+	WriteTimeout time.Duration `yaml:"write_timeout" env-default:"10s"`
 }
 
 // PostgresDb contains PostgreSQL database connection parameters.
